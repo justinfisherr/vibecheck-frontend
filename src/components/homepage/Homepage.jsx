7e066a4bbf91a950e8ca9acf7b3b2f681f831729
Alexis Martin <alexismartin1895@gmail.com>
2023-03-26 13:53:02 -0700

    fixed url

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -6,7 +6,7 @@ function Home() {
 	function redirectToSpotify() {
 		const url =
 			process.env.NODE_ENV === 'production'
-				? 'https://vibecheck-8lua.onrender.com/login'
+				? 'https://vibecheck-backend.onrender.com/login'
 				: 'http://localhost:5000/login';
 		window.location.replace(url);
 	}
