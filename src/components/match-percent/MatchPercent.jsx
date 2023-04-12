b6285ec7d5ef608158b9b5f2a3ae3f5cc0e1dd79
Alexis Martin <alexismartin1895@gmail.com>
2023-04-11 20:33:10 -0700

    fixed 100%

--- src/components/match-percent/MatchPercent.jsx
+++ src/components/match-percent/MatchPercent.jsx
@@ -3,43 +3,50 @@ import threeStar from '../../images/three-star.png';
 import './match-percent.css';
 
 export default function MatchPercent({ animationData }) {
+	const matchPercent = animationData.match_profile.match_percent;
 	return (
 		<div className='match-percent-wrapper'>
 			<img className='three-star three-star-left' src={threeStar} alt='' />
 			<img className='three-star three-star-right' src={threeStar} alt='' />
 			<p className='match-percent match-percent-1'>
-				<span className='percent'>
-					{animationData.match_profile.match_percent}
+				<span
+					className={`percent ${matchPercent === 100 ? 'one-hundred' : ''}`}>
+					{matchPercent}
 				</span>
 				<span className='percent-symbol'>%</span>
 			</p>
 			<p className='match-percent match-percent-2'>
-				<span className='percent'>
-					{animationData.match_profile.match_percent}
+				<span
+					className={`percent ${matchPercent === 100 ? 'one-hundred' : ''}`}>
+					{matchPercent}
 				</span>
 				<span className='percent-symbol'>%</span>
 			</p>
 			<p className='match-percent match-percent-3'>
-				<span className='percent'>
-					{animationData.match_profile.match_percent}
+				<span
+					className={`percent ${matchPercent === 100 ? 'one-hundred' : ''}`}>
+					{matchPercent}
 				</span>
 				<span className='percent-symbol'>%</span>
 			</p>
 			<p className='match-percent match-percent-4'>
-				<span className='percent'>
-					{animationData.match_profile.match_percent}
+				<span
+					className={`percent ${matchPercent === 100 ? 'one-hundred' : ''}`}>
+					{matchPercent}
 				</span>
 				<span className='percent-symbol'>%</span>
 			</p>
 			<p className='match-percent match-percent-5'>
-				<span className='percent'>
-					{animationData.match_profile.match_percent}
+				<span
+					className={`percent ${matchPercent === 100 ? 'one-hundred' : ''}`}>
+					{matchPercent}
 				</span>
 				<span className='percent-symbol'>%</span>
 			</p>
 			<p className='match-percent'>
-				<span className='percent'>
-					{animationData.match_profile.match_percent}
+				<span
+					className={`percent ${matchPercent === 100 ? 'one-hundred' : ''}`}>
+					{matchPercent}
 				</span>
 				<span className='percent-symbol'>%</span>
 			</p>
